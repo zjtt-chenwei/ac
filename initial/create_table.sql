f9472a24f8242b8fb5e821b58f84440a751656d1
TonyKnight13 <289992302@qq.com>
2018-01-07 14:44:14 +0800

    数据库三种表字段更新

--- initial/create_table.sql
+++ initial/create_table.sql
@@ -4,13 +4,13 @@ Navicat MySQL Data Transfer
 Source Server         : hello
 Source Server Version : 50720
 Source Host           : localhost:3306
-Source Database       : pet
+Source Database       : actestdb
 
 Target Server Type    : MYSQL
 Target Server Version : 50720
 File Encoding         : 65001
 
-Date: 2017-11-26 16:40:01
+Date: 2018-01-07 14:41:02
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -22,7 +22,7 @@ DROP TABLE IF EXISTS `user_profile`;
 CREATE TABLE `user_profile` (
   `id` int(11) NOT NULL,
   `realname` varchar(15) DEFAULT NULL,
-  `idnum` varchar(50) DEFAULT NULL,
+  `username` varchar(50) NOT NULL,
   `sex` tinyint(1) DEFAULT NULL,
   `phone` int(15) DEFAULT NULL,
   `email` varchar(150) DEFAULT NULL,
@@ -33,23 +33,19 @@ CREATE TABLE `user_profile` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Records of user_profile
--- ----------------------------
-
 /*
 Navicat MySQL Data Transfer
 
 Source Server         : hello
 Source Server Version : 50720
 Source Host           : localhost:3306
-Source Database       : pet
+Source Database       : actestdb
 
 Target Server Type    : MYSQL
 Target Server Version : 50720
 File Encoding         : 65001
 
-Date: 2017-11-26 16:40:10
+Date: 2018-01-07 14:40:49
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -60,13 +56,14 @@ SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
-  `phone` varchar(20) NOT NULL DEFAULT '' COMMENT '用户名',
-  `password` varchar(50) NOT NULL DEFAULT '' COMMENT '密码',
+  `account` varchar(20) DEFAULT NULL COMMENT '电子邮箱',
+  `password` varchar(255) NOT NULL DEFAULT '' COMMENT '密码',
   `user_profile_id` int(10) DEFAULT NULL,
-  `created` datetime DEFAULT NULL COMMENT '注册时间',
-  `changed` datetime DEFAULT NULL COMMENT '更改时间',
+  `created` datetime NOT NULL COMMENT '注册时间',
+  `changed` datetime NOT NULL COMMENT '更改时间',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户';
+
 
 /*
 Navicat MySQL Data Transfer
@@ -74,13 +71,13 @@ Navicat MySQL Data Transfer
 Source Server         : hello
 Source Server Version : 50720
 Source Host           : localhost:3306
-Source Database       : pet
+Source Database       : actestdb
 
 Target Server Type    : MYSQL
 Target Server Version : 50720
 File Encoding         : 65001
 
-Date: 2017-11-26 16:40:31
+Date: 2018-01-07 14:40:41
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -98,7 +95,3 @@ CREATE TABLE `pet` (
   `Intro` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of pet
--- ----------------------------
