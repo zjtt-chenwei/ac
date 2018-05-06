6538a2a13ce36edab09a7e8a33a925962df5631a
TonyKnight13 <289992302@qq.com>
2018-05-06 16:12:08 +0800

    单文件上传

--- controllers/upload.go
+++ controllers/upload.go
@@ -1,41 +1,41 @@
 package controllers
 
 import (
-	"actest/models"
-
-	"github.com/astaxie/beego"
+	"beegoblog/utils"
+	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
-type Uploadcontrollers struct {
+type UploadController struct {
 	BaseController
 }
 
-func (u *Uploadcontrollers) Get() {
-	u.TplName = "shangchuan.html"
-}
+func (upl *UploadController) Post() {
+	f, h, err := upl.GetFile("imgFile")
 
-func (u *Uploadcontrollers) Post() {
-	Name := u.GetString("Name")
-	Speci := u.GetString("Speci")
-	Variety := u.GetString("Variety")
-	Sex := u.GetIntWithDefault("Sex", -1)
-	Age := u.GetIntWithDefault("Age", -1)
-	Intro := u.GetString("Intro")
-	Partner := u.GetIntWithDefault("Partner", -1)
-	PetImg := u.GetString("PetImg")
+	defer f.Close()
 
-	if Sex == -1 || Age == -1 || Partner == -1 {
-		u.Ctx.WriteString("Sex || Age || Partner is illegal")
+	// 创建上传路径
+	now := time.Now()
+	dir := "./static/uploadfile/" + strconv.Itoa(now.Year()) + "/" + strconv.Itoa(int(now.Month())) + "/" + strconv.Itoa(now.Day())
+	err1 := os.MkdirAll(dir, 0755)
+	if err1 != nil {
+		upl.Data["json"] = map[string]interface{}{"error": 1, "message": "目录权限不够"}
+		upl.ServeJSON()
 		return
 	}
 
-	if Name == "" || Speci == "" || Variety == "" || Intro == "" {
-		u.Ctx.WriteString("Name or Speci or Variety or Intro is blank")
-		return
-	}
-	err := models.Pet(Name, Speci, Variety, Sex, Age, Intro, Partner, PetImg)
+	filename := h.Filename
+	ext := utils.SubString(filename, strings.LastIndex(filename, "."), 5)
+	filename = utils.GetGuid() + ext
+
 	if err != nil {
-		this.Ctx.WriteString(err.Error())
-		return
+		upl.Data["json"] = map[string]interface{}{"error": 1, "message": err}
+	} else {
+		upl.SaveToFile("imgFile", dir+"/"+filename)
+		upl.Data["json"] = map[string]interface{}{"error": 0, "url": strings.Replace(dir, ".", "", 1) + "/" + filename}
 	}
+	upl.ServeJSON()
 }
